4311340027b722c3920d695d0e1f7fe224b18f48
Akhil Rajan <rajanakhil07@gmail.com>
2023-01-06 23:18:33 +0530

    added notification cron

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/serverConfig");
+const TicketController = require("./controllers/ticket-controller");
+const jobs = require("./utils/job");
 
 const setupAndStartServer = () => {
 
@@ -10,8 +12,11 @@ const setupAndStartServer = () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
+    app.post("/api/v1/tickets", TicketController.create);
+
     app.listen(PORT, () => {
         console.log(`Server started on the part ${PORT}`);
+        jobs();
 
     });
 }
